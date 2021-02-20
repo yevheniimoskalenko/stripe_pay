23efa5ca9781feff09e01c337cdb39a7513ddadf
yevheniimoskalenko <testjokerqwerty@gmail.com>
2021-02-20 20:24:22 +0200

    second

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const chalk = require("chalk");
+// const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
 const api = require("./routers/api.routes.js");
-// const stripe = require("stripe")(process.env.sec);
 const app = express();
-// stripe.balance.retrieve((err, balance) => {
-//   console.log(balance);
-// });
+// mongoose
+//   .connect(process.env.db, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log(chalk.green(`connect has started...`)));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- src/controllers/createPay.controllers.js
+++ src/controllers/createPay.controllers.js
@@ -1,3 +1,31 @@
+const stripe = require("stripe")(process.env.sec);
 module.exports = (req, res) => {
-  console.log(req.body);
+  const { email, amount, description } = req.body;
+  stripe.customers
+    .create({
+      email,
+    })
+    .then((customer) => {
+      return stripe.invoiceItems
+        .create({
+          customer: customer.id,
+          amount: amount, // 25
+          currency: "usd",
+          description: description,
+        })
+        .then((invoiceItem) => {
+          return stripe.invoices.create({
+            collection_method: "send_invoice",
+            customer: invoiceItem.customer,
+          });
+        })
+        .then((invoice) => {
+          console.log(invoiceItem);
+
+          // New invoice created on a new customer
+        })
+        .catch((err) => {
+          // Deal with an error
+        });
+    });
 };
